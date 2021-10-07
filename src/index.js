d1d4d4df1d900c982ca8dbab7e567a4f4aa68ced
Ritikjainkkar <ritikjainkkar@gmail.com>
2021-10-07 12:02:11 +0530

    updated with new features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from './app/store'
 
 import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
